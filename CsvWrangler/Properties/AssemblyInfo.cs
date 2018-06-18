25ff4b6b445f3b2be0884ef99bf022c5dab6cad9
Maksim Libenson <maksiml@ceemax.com>
2018-06-17 23:07:16 -0700

    Update nuget

--- CsvWrangler/Properties/AssemblyInfo.cs
+++ CsvWrangler/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CsvWrangler")]
 [assembly: AssemblyDescription("CSV parser and writer.")]
 [assembly: AssemblyProduct("CsvWrangler")]
-[assembly: AssemblyCopyright("Copyright © CsvWrangler 2015")]
+[assembly: AssemblyCopyright("Copyright © CsvWrangler 2018")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
